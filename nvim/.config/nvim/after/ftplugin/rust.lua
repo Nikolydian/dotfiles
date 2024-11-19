17998a92c352f5e5b48af509bc78ed243a27987d
Nikolydian <nikolydian@gmail.com>
2024-11-19 22:47:29 +0500

    Enable rust analyzer checks in Neovim config

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,3 +1,19 @@
 vim.keymap.set("n", "<leader><leader>r", ":w !cargo run<CR>", {
     desc = "[R]un Rust App"
 })
+
+require("lspconfig").rust_analyzer.setup({
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                allFeatures = true,
+            },
+            diagnostics = {
+                enable = true,
+                experimental = {
+                    enable = true,
+                },
+            }
+        },
+    },
+})
